17a8d262e4b44ee6829db29f6ff129c6ec328390
Thang Le Dinh <thangld2407@gmail.com>
2022-05-07 08:25:05 +0700

    base init

---
+++ app.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+var path = require("path");
+
+const app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+// CONSTANTS
+const { connectToDB } = require("./database/dbConnect");
+const router = require("./router");
+const bodyParser = require("body-parser");
+
+//express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+// CORS
+app.use(cors());
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.status(400).send("Api working");
+});
+app.use("/api", router);
+app.use("*", (req, res) => {
+  res.status(404).json({
+    message: false,
+    errors: [
+      {
+        msg: "Route not found",
+      },
+    ],
+  });
+});
+
+// Collecting database , host
+const PORT = process.env.PORT || 3000;
+
+connectToDB().then((_) => {
+  app.listen(PORT, (_) => {
+    console.log(`Server started on port ${PORT}`);
+  });
+});
